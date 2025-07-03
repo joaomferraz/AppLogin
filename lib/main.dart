d8a9b0b95371ca3de9fd5a131defd2cef17b3d87
Rodrigo <rodrigocaiokell@gmail.com>
2025-07-03 09:17:14 -0300

    Apenas ajustes do nome do app

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       valueListenable: themeController.themeMode,
       builder: (context, themeMode, _) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Agenda Pessoal',
+        title: 'My Tasks',
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: themeMode,
